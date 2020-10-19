4768e462cada03f55e856c8c68ead3511c41f22e
alerner1 <alyssplerner@gmail.com>
2020-10-19 16:19:11 -0400

    nested attributes

--- app/controllers/job_leads_controller.rb
+++ app/controllers/job_leads_controller.rb
@@ -8,4 +8,19 @@ class JobLeadsController < ApplicationController
     job_lead = JobLead.find(params[:id])
     render json: job_lead
   end
+
+  def create
+    @job_lead = JobLead.create(job_leads_params)
+    if @job_lead.valid?
+      render json: {job_lead: JobLeadSerializer.new(@job_lead)}, status: :created
+    else
+      render json: { error: 'failed to create job lead' }, status: :not_acceptable
+    end
+  end
+
+  private
+
+  def job_leads_params
+    params.require(:job_leads).permit(:company, :user_id, :title, :link, :date, :contact_method, :referral, checklist_attributes: {}, contact_attributes: {}, notes_attributes: [:content])
+  end
 end
--- app/models/job_lead.rb
+++ app/models/job_lead.rb
@@ -3,4 +3,7 @@ class JobLead < ApplicationRecord
   has_one :checklist
   has_one :contact
   has_many :notes
+  accepts_nested_attributes_for :checklist
+  accepts_nested_attributes_for :contact
+  accepts_nested_attributes_for :notes
 end
