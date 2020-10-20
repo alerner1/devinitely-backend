a1bfae3f6e2e0aea7b50618499468b3d5dbc6365
alerner1 <alyssplerner@gmail.com>
2020-10-20 11:17:52 -0400

    can delete notes attributes from job lead

--- app/controllers/job_leads_controller.rb
+++ app/controllers/job_leads_controller.rb
@@ -18,9 +18,19 @@ class JobLeadsController < ApplicationController
     end
   end
 
+  def update
+    job_lead = JobLead.find(params[:id])
+    job_lead.update(job_leads_params)
+    if job_lead.save
+      render json: {job_lead: JobLeadSerializer.new(job_lead)}, status: :accepted 
+    else
+      render json: { error: 'failed to update job lead' }, status: :not_acceptable
+    end
+  end
+
   private
 
   def job_leads_params
-    params.require(:job_leads).permit(:company, :user_id, :title, :link, :date, :contact_method, :referral, checklist_attributes: {}, contact_attributes: {}, notes_attributes: [:content])
+    params.require(:job_leads).permit(:company, :user_id, :title, :link, :date, :contact_method, :referral, checklist_attributes: {}, contact_attributes: {}, notes_attributes: [:content, :id, :job_lead_id, :_destroy])
   end
 end
