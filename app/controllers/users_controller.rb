b37fa6c37ed63e1df0c7c8d369ef4ed779ca8737
alerner1 <alyssplerner@gmail.com>
2020-10-21 10:49:56 -0400

    added activities to serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,13 +18,23 @@ class UsersController < ApplicationController
     end
   end
 
-  def job_leads
-    render json: current_user.job_leads, status: :accepted
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    if user.save
+      render json: {user: UserSerializer.new(user)}, status: :accepted 
+    else
+      render json: { error: 'failed to update job lead' }, status: :not_acceptable
+    end
+  end
+
+  def users
+    render json: current_user.users, status: :accepted
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :activities)
   end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :password_digest
+  attributes :id, :first_name, :last_name, :email, :password_digest, :activities
   has_many :job_leads
 
 end
